66c6f564cd277c180f0e9ba5836fb9ba8e25e7b1
denden1s <58658954+denden1s@users.noreply.github.com>
2020-11-13 16:45:06 +0300

    Update ApplicationContext.cs

--- testing system/Classes/ApplicationContext.cs
+++ testing system/Classes/ApplicationContext.cs
@@ -30,7 +30,10 @@ namespace testing_system
         /// </summary>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=testing_system_db;Trusted_Connection=True;");
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=testing_system_db;Trusted_Connection=True;");
+            }
         }
 
         /// <summary>
@@ -41,7 +44,7 @@ namespace testing_system
             //первичные ключи
             modelBuilder.Entity<User>().HasKey(u => u.Id);
             modelBuilder.Entity<StatisticOfTest>().HasKey(k => new { k.UserID, k.TestID, k.Attempt });
-            modelBuilder.Entity<TestName>().HasKey(u =>  u.TestID);
+            modelBuilder.Entity<TestName>().HasKey(u => u.TestID);
             modelBuilder.Entity<QuestionAndAnswer>().HasKey(k => new { k.TestID, k.QuestionID });
             modelBuilder.Entity<QuestionName>().HasKey(k => new { k.QuestionID, k.TestID });
             modelBuilder.Entity<UserAnswer>().HasKey(k => new { k.TestID, k.QuestionID, k.UserID, k.Attempt });
